ed49d4036150578cdf637781917bfe6c17ca3d87
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-09-25 21:59:17 -0500

    Venenoso

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -9,7 +9,7 @@ class Anfibio(Animal):
     def __init__(self, nombre, edad, habitat, genero, colorPiel, venenoso):
         super().__init__(nombre, edad, habitat, genero, )
         self._colorPiel = colorPiel
-        self._venonoso = venenoso
+        self._venenoso = venenoso
         self._listado.append(self)
 
     @staticmethod
