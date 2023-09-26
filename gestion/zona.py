57060e741d5c07d46a7fb40d2c9a666c3a395366
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-09-25 21:53:56 -0500

    zona

--- gestion/zona.py
+++ gestion/zona.py
@@ -5,26 +5,26 @@ class Zona:
         self._zoo = zoo
         self._animales = []
 
-    def getNombre(self):
-        return self._nombre
-
     def setNombre(self, nombre):
         self._nombre = nombre
 
-    def getZoo(self):
-        return self._zoo
+    def getNombre(self):
+        return self._nombre
 
     def setZoo(self, zoo):
         self._zoo = zoo
 
-    def getAnimales(self):
-        return self._animales
+    def getZoo(self):
+        return self._zoo
 
     def setAnimales(self, animales):
         self._animales = animales
+
+    def getAnimales(self):
+        return self._animales
 
     def agregarAnimales(self, animal):
         self._animales.append(animal)
 
     def cantidadAnimales(self):
-        pass
+        return len(self._animales)
